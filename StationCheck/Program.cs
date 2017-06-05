d0e6703d390c6739eae2f1449289cf55b63589fb
Alex Innes <alexnixoninnes@gmail.com>
2017-06-05 13:02:37 +0100

    Test C# project
    First C# project - getting to know syntax and playing around with
    concepts.

---
+++ StationCheck/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace StationCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Specs Computer = new Specs();
+            double Memory = Convert.ToDouble(Computer.GetMemoryInfo()) / 1024 / 1024;
+            double roundMemory = Math.Ceiling(Memory);
+            Console.WriteLine("Drive Info:\t\t"+Computer.GetDrivesInfo());
+            Console.WriteLine("Processor Info:\t\t"+Computer.GetProcessorInfo());
+            Console.WriteLine("Memory Info:\t\t{0} GB", Convert.ToString(roundMemory));
+            Console.WriteLine("OS Info:\t\t"+Computer.GetOSInfo());
+            
+            Console.WriteLine(Computer.someNumber);
+    }
+    }
+}
---
+++ StationCheck/Specs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace StationCheck
+{
+    public class Specs
+    {
+        //Creates the list
+
+        public Specs()
+        {
+            this.SomeNumber = 1;
+        }
+
+        public string GetDrivesInfo()
+        {
+            System.IO.DriveInfo[] Drives = System.IO.DriveInfo.GetDrives();
+
+            foreach (var drive in Drives)
+            {
+                if (drive.IsReady == true)
+                {
+                    string volumeName = drive.VolumeLabel;
+                    string driveName = drive.Name;
+                    return (volumeName + " " + driveName);
+
+                }
+                else
+                {
+                    return "nope";
+                }
+            }
+            return "nope";
+
+
+        }
+
+        public object GetMemoryInfo()
+        {
+
+            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                return (result["TotalVisibleMemorySize"]);
+            }
+            return "Something went wrong";
+        }
+
+        
+        public object GetProcessorInfo()
+        {
+            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_Processor");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach(ManagementObject result in results)
+            {   
+                return (result["Name"]);
+            }
+
+            return "Something went wrong getting the processor info";
+        }
+        
+        public object GetOSInfo()
+        {
+            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+            ManagementObjectCollection results = searcher.Get();
+
+            foreach (ManagementObject result in results)
+            {
+                return (result["Caption"]);
+            }
+            return "Something went wrong";
+        }
+
+        private int SomeNumber;
+
+        public int someNumber
+        {
+            get { return SomeNumber; }
+            set { SomeNumber = value; }
+        }
+
+
+
+    }
+}
